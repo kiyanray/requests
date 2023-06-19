ec9bad87e928c5f7071c2f1c0f5b60d762d6fc00
Linus Torvalds <torvalds@linux-foundation.org>
2023-06-19 09:01:08 +0530

    [ADD] Head Method

--- example.cpp
+++ example.cpp
@@ -3,10 +3,9 @@
 
 int main() {
     std::string url = "https://api.example.com/data";
-    std::string response = HTTPClient::post(url, "key1=value1&key2=value2");
+    std::string response = Requests::get(url);
 
     if (!response.empty()) {
-        // Process the response
         std::cout << "Response: " << response << std::endl;
     } else {
         std::cout << "Request failed" << std::endl;
--- requests.h
+++ requests.h
@@ -1,13 +1,13 @@
-#ifndef HTTP_CLIENT_H
-#define HTTP_CLIENT_H
+#ifndef REQUESTS_H
+#define REQUESTS_H
 
 #include <string>
 
-class HTTPClient
-{
+class Requests {
 public:
-    static std::string get(const std::string &url);
-    static std::string post(const std::string &url, const std::string &data);
+    static std::string get(const std::string& url);
+    static std::string post(const std::string& url, const std::string& data);
+    static std::string head(const std::string& url);
 };
 
 #endif
